4afdfe0b4b49d0dfa5ff8c80d7e6e15b48ca80bd
Graham Hall <graham@ghall.dev>
2023-08-04 11:13:23 -0400

    Initial Commit

---
+++ Deck o' Cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deck o' Cards
+//
+//  Created by Graham Hall on 8/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deck o' Cards/Deck_o__CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deck_o__CardsApp.swift
+//  Deck o' Cards
+//
+//  Created by Graham Hall on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Deck_o__CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
